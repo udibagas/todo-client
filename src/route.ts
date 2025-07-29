6371b04de88f5fcc9fe11d6f212221ac4eb4db5b
Bagas Udi Sahsangka <udibagas@gmail.com>
2025-07-29 14:30:39 +0700

    update

--- src/route.ts
+++ src/route.ts
@@ -1,14 +1,36 @@
 import { createWebHistory, createRouter } from "vue-router";
+import { client } from "./lib/client";
 
 const routes = [
   { path: "/", component: () => import("./pages/Todos.vue") },
-  { path: "/register", component: () => import("./pages/Register.vue") },
-  { path: "/login", component: () => import("./pages/Login.vue") },
+  {
+    path: "/register",
+    name: "register",
+    component: () => import("./pages/Register.vue"),
+  },
+  {
+    path: "/login",
+    name: "login",
+    component: () => import("./pages/Login.vue"),
+  },
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
+});
+
+router.beforeEach(async (to, from, next) => {
+  if (to.name == "login" || to.name == "register") {
+    return next();
+  }
+
+  try {
+    await client.get("/api/me");
+    next();
+  } catch {
+    return next("/login");
+  }
 });
 
 export default router;
